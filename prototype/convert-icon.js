09d40193063c8ab32bffcee5beef74a79ca65e4a
Afsal Ali <afsalali1238@gmail.com>
2026-09-22 11:25:16 +0400

    design: switch to clean minimalist golden Bodhi leaf line art logo and favicon

--- prototype/convert-icon.js
+++ prototype/convert-icon.js
@@ -3,10 +3,10 @@ const toIco = require('png-to-ico').default || require('png-to-ico');
 
 fs.readFile('src/icon.png', (err, buffer) => {
   if (err) throw err;
-  toIco([buffer], 512)
-    .then(ico => {
-      fs.writeFileSync('src/icon.ico', ico);
-      console.log('Created src/icon.ico');
-    })
-    .catch(err => { throw err; });
+toIco('src/icon.png')
+  .then(ico => {
+    fs.writeFileSync('src/icon.ico', ico);
+    console.log('Created src/icon.ico (standard 256x256 valid Windows icon)');
+  })
+  .catch(err => { throw err; });
 });
